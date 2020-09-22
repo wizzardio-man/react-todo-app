0a36c4a3d1ea8419f89d2450d43d9123ff383e1c
Nazar Krania <nazar.krania@gmail.com>
2020-09-23 02:31:21 +0300

    Added new Submit form

---
+++ client/src/components/common/SubmitForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import { addItem } from '../../middleware/actions/itemsActions';
+import { addList } from '../../middleware/actions/listsActions';
+
+const SubmitForm = ({ addItem, addList }) => {
+    const [ term, setTerm ] = useState('');
+    const { listId } = useParams();
+
+    const submitNewTerm = ( e ) => {
+        e.preventDefault();
+        if (term === '') return;
+        if (listId) {
+            addItem(term, listId);
+        } else {
+            addList(term);
+        }
+        setTerm('');
+    }
+
+    return (
+        <div>
+            <input 
+                type='text'
+                className='form-control add-todo'
+                value={ term }
+                onChange={( e ) => { setTerm(e.target.value) }}
+            />
+            <button 
+                className='btn btn-success'
+                id='checkAll'
+                onClick={ submitNewTerm }
+            >Add</button>
+        </div>
+    )
+}
+
+const mapDispatchToProps = {
+    addItem,
+    addList
+};
+
+export default connect(null, mapDispatchToProps)(SubmitForm);
